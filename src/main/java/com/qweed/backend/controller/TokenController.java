a856e012738cf396b1bd4d3afd5f17d323e6105b
Sander <sander_nijhuis@live.nl>
2021-01-17 16:54:45 +0100

    fix(main): Fixes CrossOrigin in TokenController

--- src/main/java/com/qweed/backend/controller/TokenController.java
+++ src/main/java/com/qweed/backend/controller/TokenController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class TokenController {
     @Autowired
